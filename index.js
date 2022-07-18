fe32bc25db5f354420be1e47ccdba90f2e92147f
Jacek Dziewanowski <dziewanowski@gmail.com>
2022-07-18 14:34:27 +0200

    show version

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const version = process.env.npm_package_version;
 console.log('Hello World');
 console.log('Hello World');
 console.log('Hello World');
 console.log('Hello World');
 console.log('Hello World');
 console.log('Hello World');
+console.log('Version: ' + version);
